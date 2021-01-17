63141744a3536be12abd50143c9a5d1e1391b22f
ismaiel <ismaiel.nab@gmail.com>
2021-01-17 23:58:21 +0100

    created the initial app and start exercise one

---
+++ week1/ReactExercises/exercises/src/Hobbies/Hobbies.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hobbies = () => {
+  return (
+    <div>
+      <h1>Ismaiel Loves Louma too much</h1>
+    </div>
+  );
+};
+
+export default Hobbies;
---
+++ week1/ReactExercises/exercises/src/Hobbies/HobbyList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
+
+const HobbyList = () => {
+  return <h2> New world</h2>;
+};
+
+export default HobbyList;
