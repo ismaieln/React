3e90fbede36e201e85310d1dc1842a7398e9bc8a
ismaiel <ismaiel.nab@gmail.com>
2021-01-18 23:50:23 +0100

    complete the project

--- week1/project/hackyourweather/src/App.js
+++ week1/project/hackyourweather/src/App.js
@@ -1,8 +1,38 @@
-import City from "./City";
+import React from "react";
+import DataSource from "./city-weather.json";
+import AppTitle from "./components/AppTitle";
+import CityCard from "./components/CityCard";
+import CityName from "./components/CityName";
+import WeatherDesc from "./components/WeatherDesc";
+import WeatherDetails from "./components/WeatherDetails";
+import "./App.css";
+
 function App() {
+  const kelvinToCelsius = (kelvin) => kelvin - 273.15;
+
   return (
-    <div className="container bg-primary">
-      <h1 className="card">Ismaiel</h1>
+    <div className="App">
+      <AppTitle title="Hack your Weather" />
+
+      <div className="cards-container">
+        {DataSource.map((data, index) => {
+          return (
+            <CityCard key={index}>
+              <CityName cityName={data.name} countryCode={data.sys.country} />
+              <WeatherDesc
+                weatherState={data.weather.map((wState) => wState.main)}
+                weatherDesc={data.weather.map((wDesc) => wDesc.description)}
+              />
+              <WeatherDetails
+                minTemp={kelvinToCelsius(data.main.temp_min).toFixed(2)}
+                maxTemp={kelvinToCelsius(data.main.temp_max).toFixed(2)}
+                lat={data.coord.lat}
+                lon={data.coord.lon}
+              />
+            </CityCard>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ week1/project/hackyourweather/src/components/WeatherDetails.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function WeatherDetails({ minTemp, maxTemp, lat, lon }) {
+  return (
+    <div>
+      <div className="highlighted-info">
+        <p key={minTemp.toString(2)}>
+          <strong> min temp: </strong> {minTemp} &#8451;{" "}
+        </p>
+        <p key={maxTemp.toString(2)}>
+          <strong> max temp: </strong> {maxTemp} &#8451;{" "}
+        </p>
+        <p key={lat + lon.toString(2)}>
+          <strong> location: </strong> {lat}, {lon}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default WeatherDetails;
