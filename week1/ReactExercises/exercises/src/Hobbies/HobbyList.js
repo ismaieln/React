6196240bb4eb714c770dc1d554c2f7f8e3e56f2c
ismaiel <ismaiel.nab@gmail.com>
2021-01-19 01:10:17 +0100

    complete the React exercises

--- week1/ReactExercises/exercises/src/Hobbies/HobbyList.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const hobbies = ["Surfing", "Rock climbing", "Mountain biking", "Breakdancing"];
-
-const HobbyList = () => {
-  return <h2> New world</h2>;
-};
-
-export default HobbyList;
