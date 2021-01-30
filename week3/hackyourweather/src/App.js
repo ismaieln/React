9c1fad27cfed151b0d1036ad5fd996724dab8789
ismaiel <ismaiel.nab@gmail.com>
2021-01-31 00:31:59 +0100

    did some modification on error messages

--- week3/hackyourweather/src/App.js
+++ week3/hackyourweather/src/App.js
@@ -1,29 +1,28 @@
 import React, { useState } from "react";
 import CityCard from "./components/CityCard/CityCard";
-import Form from "./components/Form";
+import Search from "./components/Search";
 
 import "./App.css";
 
 function App() {
   const [cities, setCities] = useState([]);
-  const [hasError, setError] = useState(false);
   const [isLoading, setLoading] = useState(false);
   const [hasValue, setValue] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
 
   const fetchWeather = async (city) => {
     if (city !== "") {
       setValue(true);
       setLoading(true);
+      setErrorMessage();
       try {
         const res = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}`
         );
+
         if (!res.ok) {
-          throw new Error(
-            "The city name does not exist. Please enter a correct city name"
-          );
+          throw res.statusText;
         }
-        console.log(res.status);
         const weatherData = await res.json();
 
         const ifExist = cities.filter((el) => el.id !== weatherData.id);
@@ -31,9 +30,8 @@ function App() {
         const newCity = [weatherData, ...ifExist];
         setCities(newCity);
         setLoading(false);
-        setError(false);
       } catch (err) {
-        setError(true);
+        setErrorMessage(`There is an Error:  ${err}`);
         setLoading(false);
       }
     } else {
@@ -50,14 +48,14 @@ function App() {
       <div className="row justify-content-center">
         <h1 className="py-3 text-center">Wether</h1>
         <div className="card-size">
-          <Form onSubmit={(city) => fetchWeather(city)} />
+          <Search onSubmit={(city) => fetchWeather(city)} />
           {!hasValue && (
             <p className="fw-bold text-danger">
               The city name is empty, Please enter a correct city name
             </p>
           )}
-          {hasError && (
-            <p className="fw-bold text-danger fs-2">Something went wrong</p>
+          {errorMessage && (
+            <p className="fw-bold text-danger">{errorMessage}</p>
           )}
           {isLoading && <p className="fw-bold">Loading......</p>}
           {cities.length !== 0 && (
